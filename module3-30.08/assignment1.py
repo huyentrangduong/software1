d74f820846bb725abc5e7e8c2eaba0078aacb858
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-08-20 11:26:24 +0300

    change the filename to excercise1

--- module3-30.08/assignment1.py
+++
@@ -1,8 +0,0 @@
-##https://metropolia-sw.github.io/sw1-python/en/03_variables_and_interactive_programs.html
-##model: https://moodle.metropolia.fi/course/view.php?id=10978
-Key: Python_TXL26S1-A
-
-
-name = input("Give name: ")
-greeting = "Hello, "+ name +"!"
-print(greeting) #Viivi
