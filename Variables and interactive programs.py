1d17bdfe7237dcd61a6290b0fd879b62a627f7ce
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-08-20 10:10:37 +0300

    add new files and chang some files

---
+++ Variables and interactive programs.py
@@ -0,0 +1,19 @@
+
+print("hello Viet Nam!")
+
+print("I'm Jane")
+
+print(5)
+print(10)
+
+print("good")
+print("morning")
+print("good\nmorning") #print the seperate row for each word
+print("good \n morning")
+
+name = input("name: ")
+
+print(name)
+print("name")
+
+print("you're name is "+name)
---
+++ module3-30.08/assignment1.py
@@ -0,0 +1,8 @@
+##https://metropolia-sw.github.io/sw1-python/en/03_variables_and_interactive_programs.html
+##model: https://moodle.metropolia.fi/course/view.php?id=10978
+Key: Python_TXL26S1-A
+
+
+name = input("Give name: ")
+greeting = "Hello, "+ name +"!"
+print(greeting) #Viivi
