663cc61fdb5f73b1181e93238a1437ac91558d60
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-08-25 14:42:45 +0300

    remove the content of #

--- module3-30.08/excercise5.py
+++ module3-30.08/excercise5.py
@@ -8,9 +8,8 @@ lots = input("Enter lots: ")
 lots = float(lots)
 
 total_grams = (((talents*20)+ pounds)*32 + lots)*13.3
-#total_grams = int(total_grams)
 
-kilograms = int(total_grams//1000)  #convert gr to kg
+kilograms = int(total_grams//1000) 
 remaining_grams = (total_grams%1000)
 
 print("The weight in modern units: ")
