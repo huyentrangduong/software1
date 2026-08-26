c3a7e857f9d1e17f7cd3eb29b3849541a1768b35
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-08-26 12:59:05 +0300

    add exercise2

---
+++ module4/exercise2.py
@@ -0,0 +1,17 @@
+#LUX: upper-deck cabin with a balcony.
+#A: above the car deck, equipped with a window.
+#B: windowless cabin above the car deck.
+#C: windowless cabin below the car deck.
+
+cabin_class = input ("cabin_class: ")
+
+if cabin_class == "LUX":
+    print("Enter the cabin class (LUX, A, B, or C): " + cabin_class)
+elif cabin_class == "A":
+    print("Enter the cabin class (LUX, A, B, or C): " + cabin_class)
+elif cabin_class == "B":
+    print("Enter the cabin class (LUX, A, B, or C): " + cabin_class)
+elif cabin_class == "C":
+    print("Enter the cabin class (LUX, A, B, or C): " + cabin_class)
+else:
+    print("Invalid cabin class.")
