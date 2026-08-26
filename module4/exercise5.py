d99543b16b62dd1336c7974fc2e4f5881b418051
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-08-26 18:57:53 +0300

    complete exercise 5

--- module4/exercise5.py
+++ module4/exercise5.py
@@ -1,8 +1,10 @@
 year = int(input("Enter a year: "))
-
-if year%4 or year%100:
+    
+if year%4 == 0 and year%100 !=0:     #2020
     print(f"{year} is a leap year.")
-elif year%100 and year%400:
-    print(f"{year} is a not leap year.")
+    
+elif year%4 == 0 and year%100 == 0 and year%400 == 0:  #2000
+    print(f"{year} is a leap year.")                    
+    
 else:
     print(f"{year} is not a leap year.")
