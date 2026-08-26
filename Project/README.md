55c37473c632beb26d2f170b8b2a1c9fcb4dd941
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-08-26 12:59:58 +0300

    add project folder

---
+++ Project/README.md
@@ -0,0 +1,2 @@
+#tictactoe game
+Duong, Huyen Trang
---
+++ Project/program.py
@@ -0,0 +1,3 @@
+player_name = input("name of gamer: ")
+player_age = input("age of gamer: ")
+print("gamer's information: ",player_name, player_age)
