abe75ae5684ae9898f74476456c4755d1ee60182
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-08-30 20:55:22 +0300

    add exercise 3

---
+++ module5/exercise2.py
@@ -0,0 +1,10 @@
+
+inches = float(input("Enter length in inches (negative value to quit): "))
+
+while  inches >= 0:
+    centimeters = inches * 2.54     #1 inch = 2,54cm 
+    print (f'{inches} inches is {centimeters:.2f} centimeters')
+    
+    inches = float(input("Enter length in inches (negative value to quit): "))
+
+print("Program ended.")
