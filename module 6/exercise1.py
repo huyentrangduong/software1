fc201085945b1fdcea85217b34ee574c3186c585
huyentrangduong <duonghuyentrang12121991@gmail.com>
2026-09-02 15:36:07 +0300

    complete ex1 of module6

--- module 6/exercise1.py
+++ module 6/exercise1.py
@@ -1,12 +1,15 @@
-#Write a program that asks the user how many dice to roll. The program rolls all the dice once and prints out the sum of the numbers. 
+#Write a program that asks the user how many dice to roll. 
+#The program rolls all the dice once and prints out the sum of the numbers. 
 #Use a for loop. The program should ask the user for input with the following text: How many dice to roll:
 #The program should print exactly the following text (example if the user inputs the number 5): Sum of the dice: 15
 
 import random
-rolls = int(input("How many dice to roll: "))
-
+dice = int(input("How many dice to roll: "))
 sum = 0
 
-for roll in range(rolls):
-    sum = sum + random.randint(1,9)
-    print(f"Sum of the dice: {sum}")
+
+for each_roll in range(dice):
+    each_roll = random.randint(1,6)
+    sum = sum + each_roll
+
+print(f"Sum of the dice: {sum}")
